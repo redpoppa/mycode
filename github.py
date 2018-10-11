4750a0095f7275bc6f74bbbaaaa7274d77e25888
James Redman <jamesjred@gmail.com>
2018-10-11 16:32:13 -0400

    my commit message

---
+++ github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import os
+import time
+
+os.system("git add *")
+
+
+time.sleep(1)
+
+os.system('git commit -m "my commit message"')
+
+
+time.sleep(5)
+
+
+
+os.system('git push origin master')
+
+
+
